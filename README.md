59926747b7498af14e4cbe52fa4ca08fc1571895
Michael Heinrichs <mheinrichs@gmail.com>
2023-06-06 17:43:54 -0400

    Modified the script to search by ISSN

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# doajm
-Extract specific fields from doaj.org api
+# doaj-issn-lookup
+Extract specific fields from doaj.org API, searching for ISSN values provided in a CSV file 
--- doajm.py
+++ doajm.py
@@ -11,20 +11,19 @@ import csv
 import time
 from sys import argv
 
-URL = 'https://doaj.org/api/v1/search/journals/' #URL to API'et
+URL = 'https://doaj.org/api/search/journals/' #URL to API'et
 INPUT_FILE = argv[1] #1st. argument containing name of input file.
 OUTPUT_FILE = argv[2] #2nd. argument containing name of output file.
 SEARCHF = argv[3] #Seatchterm with quotationmarks or without.
 
-def pull_data_api(url):
+def pull_data_api(issn):
     """Execute request in API and print either found data or errorcode to terminal."""
+    url = URL + 'issn:' + issn
     req = urllib.request.Request(url)
     try:
         response = urllib.request.urlopen(req)
         data = json.loads(response.read())
-        write_data(OUTPUT_FILE, data)
-        print(data)
-        time.sleep(0.5)
+        write_data(OUTPUT_FILE, issn, data)
 
     except urllib.error.HTTPError as _:
         print(_.reason)
@@ -32,18 +31,20 @@ def pull_data_api(url):
 def open_csv():
     """Open file with name INPUT_FILE and calls the function pull_data_api."""
     with open(INPUT_FILE, newline='') as _:
-        reader = csv.reader(_, delimiter=';')
-        next(_)
+        reader = csv.reader(_, delimiter=',')
         for row in reader:
-            pull_data_api(URL + row[0])
+            print(row)
+            pull_data_api(row[0])
 
-def write_data(json_name, data):
+def write_data(output_filename, issn, data):
     """Write data to file with name OUTPUT_FILE (append mode)."""
-    with open(json_name, mode="a") as file:
+    with open(output_filename, mode="a") as file:
         try:
-            file.write(data['results'][0]['bibjson'][SEARCHF]+'\n')
+            file.write(issn + ',')
+            file.write(data['results'][0]['bibjson'][SEARCHF])
         except IndexError as _:
             pass
+        file.write('\n')
 
 def main():
     """main"""
